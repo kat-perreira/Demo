daec15f7f240e95b0843ac99e72b4b6e9b91f688
Kat Perreira ♡ <kat.perreira@highspot.com>
2022-06-27 12:38:08 -0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -6,3 +6,5 @@ const newThing = "a thing ive never seen before"
 const oldWay = "old way of doing something"
 
 const coolThing = "something new and clever that deserves praise"
+
+const strangeThing = "Im an unknown thing"
