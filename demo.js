ff472fb5e5fb0b16dcdff500fa7a3ccb76647ced
Kat Perreira ♡ <kat.perreira@highspot.com>
2022-06-27 12:04:22 -0700

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,8 @@
+const something = "im a bad thing"
+const breakingChange = something.notValid
+
+const newThing = "a thing ive never seen before"
+
+const oldWay = "old way of doing something"
+
+const coolThing = "something new and clever that deserves praise"
